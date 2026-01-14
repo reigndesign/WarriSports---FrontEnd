8370f5731512f6509d4cc7f1d0ad820317b8ab9f
Adetunji Adeleye <igbengoos@hotmail.com>
2026-01-14 12:11:23 +0100

    commit this day 3

--- web/clients/assets/js/clients.js
+++ web/clients/assets/js/clients.js
@@ -1,4 +1,15 @@
 // Navbar User Onclick Func
-function clickProfileMenu(){           
-            document.getElementById("Menu").classList.toggle("hidden");                       
-       }
+function clickProfileMenu(){ 
+  document.getElementById("Menu").classList.toggle("hidden");                       
+}
+
+// Sidebar User Onclick Func
+
+const menuBtn = document.getElementById('menuBtn');
+const sideNav = document.getElementById('sideNav');
+
+menuBtn.addEventListener('click', () => {
+  sideNav.classList.toggle('hidden');
+
+
+});
