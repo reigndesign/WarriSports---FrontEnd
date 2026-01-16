c952bf472b3800f8c8dc07c177ae00251abe2d45
Adetunji Adeleye <igbengoos@hotmail.com>
2026-01-16 02:37:20 +0100

    commit js

--- web/clients/assets/js/clients.js
+++ web/clients/assets/js/clients.js
@@ -9,6 +9,7 @@ const menuBtn = document.getElementById('menuBtn');
 const sideNav = document.getElementById('sideNav');
 
 menuBtn.addEventListener('click', () => {
+  
   sideNav.classList.toggle('hidden');
 
 
