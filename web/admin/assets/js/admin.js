6b9c511b87cb9286df765be378918743244bdb6a
Adetunji Adeleye <igbengoos@hotmail.com>
2026-01-17 15:57:34 +0100

    Commit this progress

---
+++ web/admin/assets/js/admin.js
@@ -0,0 +1,16 @@
+// Navbar User Onclick Func
+function clickProfileMenu(){ 
+  document.getElementById("Menu").classList.toggle("hidden");                       
+}
+
+// Sidebar User Onclick Func
+
+const menuBtn = document.getElementById('menuBtn');
+const sideNav = document.getElementById('sideNav');
+
+menuBtn.addEventListener('click', () => {
+  
+  sideNav.classList.toggle('hidden');
+
+
+});
